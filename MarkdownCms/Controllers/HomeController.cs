e35e0bf5d525591cf6f7a48dd4a8606bfe1b1c60
pgrzmil <pawel.grzmil@hotmail.com>
2015-06-07 23:40:43 +0200

    Generic routing resolved

--- MarkdownCms/Controllers/HomeController.cs
+++ MarkdownCms/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using MarkdownCms.Helpers;
+using MarkdownCms.Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -12,17 +13,76 @@ namespace MarkdownCms.Controllers
     {
         public ActionResult Index(string path = "")
         {
-            var rootPath = ConfigurationManager.AppSettings["FilesPath"];
-            var tree = DirectoryHelper.GetDirectoryTree(rootPath);
+            var tree = DirectoryHelper.GetDirectoryTree(ConfigurationManager.AppSettings["FilesPath"]);
             ViewBag.Nodes = tree;
 
-            using (var reader = new System.IO.StreamReader(rootPath + "\\README.md"))
+            string filePath = "";
+            if (String.IsNullOrEmpty(path))
+            {
+                filePath = GetDefaultFilePath(tree);
+                if (String.IsNullOrEmpty(filePath))
+                    return View();
+            }
+            else
+                filePath = GetFilePath(tree, path);
+
+            if (!String.IsNullOrEmpty(filePath))
+            {
+                ViewBag.FileContent = GetHtmlFromFile(filePath);
+                return View();
+            }
+            else
+            {
+                return View("MessageView", "File or directory can not be found" as object);
+            }
+        }
+
+        private string GetHtmlFromFile(string filePath)
+        {
+            string htmlResult = "";
+            using (var reader = new System.IO.StreamReader(filePath))
             {
                 var fileContent = reader.ReadToEnd();
-                var htmlResult = CommonMark.CommonMarkConverter.Convert(fileContent);
-                ViewBag.FileContent = htmlResult;
+                htmlResult = CommonMark.CommonMarkConverter.Convert(fileContent);
             }
-            return View();
+            return htmlResult;
+        }
+
+        private string GetFilePath(IList<DirectoryNode> tree, string path)
+        {
+            var filePath = "";
+            var parts = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            if (parts.Count != 0)
+            {
+                var node = tree.FirstOrDefault(x => x.Name == parts[0]);
+                if (node != null)
+                {
+                    if (parts.Count != 1)
+                    {
+                        for (int i = 1; i < parts.Count; i++)
+                        {
+                            node = node.Subfolders.FirstOrDefault(x => x.Name == parts[i]);
+                            if (node != null && i == parts.Count - 1)
+                            {
+                                filePath = node.IsFile ? node.Path : GetDefaultFilePath(node.Subfolders);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        filePath = node.IsFile ? node.Path : GetDefaultFilePath(node.Subfolders);
+                    }
+                }
+            }
+
+            return filePath;
+        }
+
+        private string GetDefaultFilePath(IList<DirectoryNode> tree)
+        {
+            var node = tree.FirstOrDefault(f => f.IsFile && (f.Name.ToLower() == "index" || f.Name.ToLower() == "readme"));
+            return node != null ? node.Path : String.Empty;
         }
     }
 }
