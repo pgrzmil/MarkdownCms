163a9aae6d9369aab76f8664bcfa47dd5116a44b
Marcin Badurowicz <m@badurowicz.net>
2015-08-20 17:02:50 +0200

    first try to move to ASP.NET 5

---
+++ src/MarkdownCms/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using MarkdownCms.Helpers;
+using MarkdownCms.Models;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarkdownCms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string path = "")
+        {
+            //var tree = DirectoryHelper.GetDirectoryTree(ConfigurationManager.AppSettings["FilesPath"]);
+            var tree = DirectoryHelper.GetDirectoryTree(".");
+            ViewBag.Nodes = tree;
+
+            string filePath = "";
+            if (String.IsNullOrEmpty(path))
+            {
+                filePath = GetDefaultFilePath(tree);
+                if (String.IsNullOrEmpty(filePath))
+                    return View();
+            }
+            else
+                filePath = GetFilePath(tree, path);
+
+            if (!String.IsNullOrEmpty(filePath))
+            {
+                ViewBag.FileContent = GetHtmlFromFile(filePath);
+                return View();
+            }
+            else
+            {
+                return View("MessageView", "File or directory can not be found" as object);
+            }
+        }
+
+        private string GetHtmlFromFile(string filePath)
+        {
+            string htmlResult = "";
+
+            var fileContent = System.IO.File.ReadAllText(filePath);
+            htmlResult = CommonMark.CommonMarkConverter.Convert(fileContent);
+
+            return htmlResult;
+        }
+
+        private string GetFilePath(IList<DirectoryNode> tree, string path)
+        {
+            var filePath = "";
+            var parts = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            if (parts.Count != 0)
+            {
+                var node = tree.FirstOrDefault(x => x.Name == parts[0]);
+                if (node != null)
+                {
+                    if (parts.Count != 1)
+                    {
+                        for (int i = 1; i < parts.Count; i++)
+                        {
+                            node = node.Subfolders.FirstOrDefault(x => x.Name == parts[i]);
+                            if (node != null && i == parts.Count - 1)
+                            {
+                                filePath = node.IsFile ? node.Path : GetDefaultFilePath(node.Subfolders);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        filePath = node.IsFile ? node.Path : GetDefaultFilePath(node.Subfolders);
+                    }
+                }
+            }
+
+            return filePath;
+        }
+
+        private string GetDefaultFilePath(IList<DirectoryNode> tree)
+        {
+            var node = tree.FirstOrDefault(f => f.IsFile && (f.Name.ToLower() == "index" || f.Name.ToLower() == "readme"));
+            return node != null ? node.Path : String.Empty;
+        }
+    }
+}
---
+++ src/MarkdownCms/Models/DirectoryNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarkdownCms.Models
+{
+    public class DirectoryNode
+    {
+        public DirectoryNode()
+        {
+            this.Subfolders = new List<DirectoryNode>();
+        }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public IList<DirectoryNode> Subfolders { get; set; }
+
+        public bool IsFile { get; set; }
+
+        public String GetPathForUrl()
+        {
+            //var rootDirectory = ConfigurationManager.AppSettings["FilesPath"];
+            var rootDirectory = ".";
+            var path = this.Path.Replace(rootDirectory, "");
+            path = path.Replace('\\', '/');
+            var index = path.LastIndexOf('.');
+
+            path = index > 0 ? path.Substring(0, index) : path;
+            return path;
+        }
+    }    
+}
