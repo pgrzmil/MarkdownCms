5537ef85fcf781fd255959557fcf4786216370b8
Marcin Badurowicz <m@badurowicz.net>
2015-08-20 21:32:28 +0200

    modifying routing to match old version

--- src/MarkdownCms/Startup.cs
+++ src/MarkdownCms/Startup.cs
@@ -103,7 +103,9 @@ namespace MarkdownCms
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{*path}",
+                    defaults: new { controller = "Home", action = "Index" }                
+            );
 
                 // Uncomment the following line to add a route for porting Web API 2 controllers.
                 // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
